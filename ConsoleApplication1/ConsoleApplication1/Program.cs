a4062dba6ab4b8d78aa66570f2831c0690f952ed
brian_huang <brian_huang@trend.com.tw>
2016-01-04 17:42:25 +0800

    upload by webber

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var orderList = new OrderList();
+
+
+            orderList.AddOrder(new Order { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21 });
+            orderList.AddOrder(new Order { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22 });
+            orderList.AddOrder(new Order { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23 });
+            orderList.AddOrder(new Order { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24 });
+            orderList.AddOrder(new Order { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25 });
+            orderList.AddOrder(new Order { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26 });
+            orderList.AddOrder(new Order { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27 });
+            orderList.AddOrder(new Order { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28 });
+            orderList.AddOrder(new Order { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29 });
+            orderList.AddOrder(new Order { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30 });
+            orderList.AddOrder(new Order { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31 });
+            orderList.AddOrder(new Order { Id = 12, Cost = 12, Revenue = 22, SellPrice = 32 });
+
+            orderList.Sum("Id", Int32.MaxValue);
+            orderList.Sum("Cost", 3);
+            orderList.Sum("Revenue", 4);
+            orderList.Sum("SellPrice", 1);
+
+            orderList.Sum("wrong", 4);
+            orderList.Sum(null, 4);
+            orderList.Sum("SellPrice", 0);
+            orderList.Sum("SellPrice", -100);
+            orderList.Sum("SellPrice", Int32.MinValue);
+
+            var expected = new List<int>(){100,200,300};
+            Console.ReadKey();
+        }
+    }
+
+    public class OrderList
+    {
+        public OrderList()
+        {
+             _OrderList= new List<Order>();
+        }
+        public int AddOrder(Order order)
+        {
+            //Id should be unique
+            _OrderList.Add(order);
+
+            return 0;
+        }
+        public List<int> Sum(string name, int groupCount)
+        {
+            Console.WriteLine("###########################");
+
+            var result = new List<int>();
+
+            if (0 >= groupCount)
+            {
+                Console.WriteLine("bad groupCount.");
+                return result;
+            }
+
+            if (null == name)
+            {
+                Console.WriteLine("no such name.");
+                return result;
+            }
+            Type myType = typeof(Order);
+            PropertyInfo myPropInfo = myType.GetProperty(name);
+            if (null == myPropInfo) {
+                Console.WriteLine("no such name.");
+                return result;
+            }
+            int index = 1;
+            int temp = 0;
+            foreach (var order in _OrderList)
+            {
+                temp += order.getValueByName(name);
+                if ((1 < index || 1== groupCount) && 0 == ((index) % groupCount))
+                {
+                    result.Add(temp);
+                    Console.WriteLine(temp);
+                    temp = 0;
+                }
+                index++;
+            }
+            if (0 != ((index - 1) % groupCount))
+            {
+                result.Add(temp);
+                Console.WriteLine(temp);
+                   
+            }
+            return result;
+        }
+
+
+        private List<Order> _OrderList { get; set; }
+    }
+    public class Order
+    {
+
+        public int Id { get; set; }
+
+        public int Cost { get; set; }
+        public int Revenue { get; set; }
+
+        public int SellPrice { get; set; }
+
+        public int getValueByName(string propertyName)
+        {
+            Type myType = typeof(Order);
+            PropertyInfo myPropInfo = myType.GetProperty(propertyName);
+            if (null == myPropInfo) return 0;
+
+            var value =  myPropInfo.GetValue(this, null);
+
+            string str = null==value ? "" : value.ToString();
+            int j=0;
+            if (Int32.TryParse(str, out j))
+            {
+                //Console.WriteLine(j);
+            }
+            else
+                Console.WriteLine("String could not be parsed.");
+
+            return j;
+        }
+    }
+
+}
---
+++ ConsoleApplication1/ConsoleApplication1Tests/OrderListTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace ConsoleApplication1.Tests
+{
+    [TestClass()]
+    public class OrderListTests
+    {
+        OrderList orderList;
+        /*
+        [ClassInitialize()]
+        */
+        public void InitializeOrderList()
+        {
+            orderList = new OrderList();
+
+
+            orderList.AddOrder(new Order { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21 });
+            orderList.AddOrder(new Order { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22 });
+            orderList.AddOrder(new Order { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23 });
+            orderList.AddOrder(new Order { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24 });
+            orderList.AddOrder(new Order { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25 });
+            orderList.AddOrder(new Order { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26 });
+            orderList.AddOrder(new Order { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27 });
+            orderList.AddOrder(new Order { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28 });
+            orderList.AddOrder(new Order { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29 });
+            orderList.AddOrder(new Order { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30 });
+            orderList.AddOrder(new Order { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31 });
+            //orderList.AddOrder(new Order { Id = 12, Cost = 12, Revenue = 22, SellPrice = 32 });
+        }
+       
+        [TestMethod()]
+        public void SumTest_Cost_3()
+        {
+            InitializeOrderList();
+
+
+            List<int> actual = orderList.Sum("Cost", 3);
+            var expected = new List<int>() { 6, 15, 24, 21 };
+            CollectionAssert.AreEqual(actual, expected);
+             //Assert.Fail();
+
+        }
+        public void SumTest_Revenue_4() 
+        {
+            InitializeOrderList();
+            List<int> actual = orderList.Sum("Revenue", 4);
+            var expected = new List<int>() { 50, 66, 60 };
+            CollectionAssert.AreEqual(actual, expected);
+        }
+        public void SumTest_SellPrice_1()
+        {
+            List<int> actual = orderList.Sum("SellPrice", 1);
+            var expected = new List<int>() { 50, 66, 60 };
+            CollectionAssert.AreEqual(actual, expected);
+        }
+        public void SumTest_Id_max()
+        {
+            List<int> actual = orderList.Sum("Id", Int32.MaxValue);
+            var expected = new List<int>() { 50, 66, 60 };
+            CollectionAssert.AreEqual(actual, expected);
+        }
+    }
+}
+/*
+orderList.Sum("Id", Int32.MaxValue);
+orderList.Sum("Cost", 3);
+orderList.Sum("Revenue", 4);
+orderList.Sum("SellPrice", 1);
+
+orderList.Sum("wrong", 4);
+orderList.Sum(null, 4);
+orderList.Sum("SellPrice", 0);
+orderList.Sum("SellPrice", -100);
+orderList.Sum("SellPrice", Int32.MinValue);
+*/
